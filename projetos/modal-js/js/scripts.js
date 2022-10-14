b943ea5c1e15ea1f2cba2beb233d4d78fc2510ac
Front.neto <antoniocruznb@gmail.com>
2022-10-14 14:41:13 -0300

    Add files via upload

---
+++ projetos/modal-js/js/scripts.js
@@ -0,0 +1,18 @@
+// JAVASCRIPT
+
+// Armazenando as variaveis necessárias
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+
+//criando função para abir e fechar modal
+const toggleModal = () => {
+  modal.classList.toggle("hide");
+  fade.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
